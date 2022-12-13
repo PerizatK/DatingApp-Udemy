ca07ca6e61e41e1c58265a0ce1ff34e8be4124bc
Perizat Karina <Perizat KarinaPerizat>
2022-12-13 20:32:25 +0600

    End of Section 2

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -28,27 +30,32 @@ namespace API
         {
 
             services.AddControllers();
-            //services.AddSwaggerGen( c =>
-            // {
-            //     c.SwaggerDoc( "v1", new OpenApiInfo { Title = "DatingApp", Version = "v1" } );
-            // } );
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddSwaggerGen( c =>
+             {
+                 c.SwaggerDoc( "v1", new OpenApiInfo { Title = "DatingApp", Version = "v1" } );
+             } );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure( IApplicationBuilder app, IWebHostEnvironment env )
         {
-            //if ( env.IsDevelopment() )
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //    app.UseSwagger();
-            //    app.UseSwaggerUI( c => c.SwaggerEndpoint( "/swagger/v1/swagger.json", "DatingApp v1" ) );
-            //}
+            if ( env.IsDevelopment() )
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI( c => c.SwaggerEndpoint( "/swagger/v1/swagger.json", "DatingApp v1" ) );
+            }
 
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
-            //app.UseRouting();
+            app.UseRouting();
 
-            //app.UseAuthorization();
+            app.UseAuthorization();
 
             app.UseEndpoints( endpoints =>
              {
